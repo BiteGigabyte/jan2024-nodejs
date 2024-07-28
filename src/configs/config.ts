7ed769eadcd0389f5894c82051f34183a05cdcc8
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-07-28 23:55:22 +0300

    переписав урок 7

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -6,10 +6,15 @@ export const configs = {
   APP_PORT: Number(process.env.APP_PORT),
   APP_HOST: process.env.APP_HOST,
 
+  FRONTEND_URL: process.env.FRONTEND_URL,
+
   MONGO_URL: process.env.MONGO_URL,
 
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
   JWT_ACCESS_EXPIRE_IN: process.env.JWT_ACCESS_EXPIRE_IN,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   JWT_REFRESH_EXPIRE_IN: process.env.JWT_REFRESH_EXPIRE_IN,
+
+  SMTP_EMAIL: process.env.SMTP_EMAIL,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
 };
